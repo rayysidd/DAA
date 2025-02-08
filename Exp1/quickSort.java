e5166fdc7e9629ea4d3ec9f81b2194171f23ee59
rayysidd <siddiqui.rayyan@gmail.com>
2025-02-08 11:25:26 +0530

    Added quick sort

---
+++ Exp1/quickSort.java
@@ -0,0 +1,44 @@
+public class quickSort {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 5, 1, 2, 3, 9, 4 };
+        quickSort(arr, 0, arr.length - 1);
+
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+
+    public static void quickSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int pivot = partition(arr, l, r);
+            quickSort(arr, pivot + 1, r);
+            quickSort(arr, l, pivot - 1);
+        }
+    }
+
+    public static int partition(int[] arr, int l, int r) {
+        int pivot = arr[l];
+        int i = l;
+        int j = r;
+
+        while (i < j) {
+            while (arr[i] <= pivot && i <= r - 1) {
+                i++;
+            }
+            while (arr[j] >= pivot && j >= l + 1) {
+                j--;
+            }
+
+            if (i < j) {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        int temp = arr[l];
+        arr[l] = arr[j];
+        arr[j] = temp;
+
+        return j;
+    }
+}
--- Exp1/sorting.java
+++ Exp1/sorting.java
@@ -15,6 +15,8 @@ public class sorting {
             System.out.println("Selection Sort Time: " + measureSortingTime(randomArray, "Selection") + " ms");
             System.out.println("Insertion Sort Time: " + measureSortingTime(randomArray, "Insertion") + " ms");
             System.out.println("Tournament Sort Time: " + measureSortingTime(randomArray, "Tournament") + " ms");
+            System.out.println("Quick Sort Time: " + measureSortingTime(randomArray, "Quick") + " ms");
+
         }
     }
 
@@ -43,6 +45,9 @@ public class sorting {
                 break;
             case "Tournament":
                 tournamentSort(copy);
+                break;
+            case "Quick":
+                quickSort(copy, 0, copy.length - 1);
                 break;
         }
         long endTime = System.nanoTime();
@@ -145,5 +150,39 @@ public class sorting {
 
             i = smallest;
         }
+    }
+
+    public static void quickSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int pivot = partition(arr, l, r);
+            quickSort(arr, pivot + 1, r);
+            quickSort(arr, l, pivot - 1);
+        }
+    }
+
+    public static int partition(int[] arr, int l, int r) {
+        int pivot = arr[l];
+        int i = l;
+        int j = r;
+
+        while (i < j) {
+            while (arr[i] <= pivot && i <= r - 1) {
+                i++;
+            }
+            while (arr[j] >= pivot && j >= l + 1) {
+                j--;
+            }
+
+            if (i < j) {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        int temp = arr[l];
+        arr[l] = arr[j];
+        arr[j] = temp;
+
+        return j;
     }
 }
